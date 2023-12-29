1ac00bf235041b69f3928eb553f4d2d0959176be
Nisanth Chunduru <nisanth074@gmail.com>
2023-12-29 17:54:04 +0530

    Parallelize scraping

--- scrape.py
+++ scrape.py
@@ -4,8 +4,13 @@ from urllib.parse import urlparse
 from datetime import datetime, timedelta
 import time
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor, as_completed, wait, FIRST_COMPLETED
 
 class Scraper:
+  PARALLELISM = 2
+  DOMAIN_MAX_ACCEPTABLE_502_COUNT = 2
+  RETRY_DELAY_AFTER_502 = 60
+
   def __init__(self, process_page):
     self.process_page = process_page
 
@@ -21,11 +26,30 @@ class Scraper:
   def scrape(self):
     self.processed_urls_file = open('processed_urls.csv', 'w')
     self.bad_urls_file = open('bad_urls.csv', 'w')
-    max_acceptable_domain_502s = 2
-    wait_period_after_502 = 60
-    # wait_period_after_502 = 5
+    executor = ThreadPoolExecutor(max_workers=self.PARALLELISM)
+    self.workers = {}
 
-    while not self.urls.empty():
+    while True:
+      if len(self.workers) >= self.PARALLELISM:
+        # Wait for a worker to become done
+        # as_completed(self.workers.values())
+        wait(self.workers.values(), None, return_when=FIRST_COMPLETED)
+        next
+
+      if self.urls.empty():
+        if len(self.workers) == 0:
+          break
+        else:
+          # Wait for atleast on worker to be done
+          # as_completed(self.workers.values())
+          wait(self.workers.values(), None, return_when=FIRST_COMPLETED)
+          continue
+      else:
+          if len(self.workers) == self.PARALLELISM:
+            # Wait for atleast one worker to be done
+            # as_completed(self.workers.values())
+            wait(self.workers.values(), None, return_when=FIRST_COMPLETED)
+
       priority, url = self.urls.get()
       scrape_at = datetime.fromtimestamp(priority)
       current_time = datetime.now()
@@ -34,47 +58,65 @@ class Scraper:
 
       parsed_url = urlparse(url)
       url_domain = parsed_url.netloc
-      url_path = parsed_url.path
 
       if url_domain in self.five02_domains:
-        unreachable_until = self.five02_domains[url_domain]
-        if self.domain_502_counts[url_domain] >= max_acceptable_domain_502s:
-          reason = "max_acceptable_domain_502s_reached"
-          self._add_url_to_processed_urls_file(url, reason)
-          next
-        elif datetime.now() < unreachable_until:
-          self._scrape_url_at(url, unreachable_until)
-          next
-
-      response = requests.get(url)
-
-      if response.status_code == 200:
-        try:
-          self.process_page(response.text, url_domain, url_path)
-        except Exception as e:
-          exception_name = e.__class__.__name__
-          reason = exception_name
-          self._add_url_to_bad_urls_file(reason)
-          next
-
-        self._add_url_to_processed_urls_file(url)
-      elif response.status_code == 502:
-        self.domain_502_counts[url_domain] = self.domain_502_counts[url_domain] + 1
-
-        if self.domain_502_counts[url_domain] >= max_acceptable_domain_502s:
-          reason = 502
+        retry_at = self.five02_domains[url_domain]
+        if self.domain_502_counts[url_domain] >= self.DOMAIN_MAX_ACCEPTABLE_502_COUNT:
+          reason = "domain_max_acceptable_502_count_reached"
           self._add_url_to_bad_urls_file(url, reason)
-        else:
-          self._scrape_url_in(url, wait_period_after_502)
-      else:
-        reason = response.status_code
-        self._add_url_to_bad_urls_file(url, reason)
+          continue
+        elif datetime.now() < retry_at:
+          self._scrape_url_at(url, retry_at)
+          continue
+
+      worker = executor.submit(self._get_url, url)
+      worker.add_done_callback(self._after_url_get)
+      self.workers[url] = worker
 
     self.processed_urls_file.close()
     self.bad_urls_file.close()
 
-  def _formatted_current_time(self):
-    return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+  def _get_url(self, url):
+    response = requests.get(url)
+    return (url, response)
+
+  def _after_url_get(self, worker):
+    url, response = worker.result()
+
+    del self.workers[url]
+
+    parsed_url = urlparse(url)
+    url_domain = parsed_url.netloc
+    url_path = parsed_url.path
+
+    if response.status_code == 200:
+      try:
+        self.process_page(response.text, url_domain, url_path)
+      except Exception as e:
+        exception_name = e.__class__.__name__
+        reason = exception_name
+        self._add_url_to_bad_urls_file(reason)
+        next
+
+      self._add_url_to_processed_urls_file(url)
+    elif response.status_code == 502:
+      self.domain_502_counts[url_domain] = self.domain_502_counts[url_domain] + 1
+
+      if self.domain_502_counts[url_domain] >= self.DOMAIN_MAX_ACCEPTABLE_502_COUNT:
+        reason = 502
+        self._add_url_to_bad_urls_file(url, reason)
+      else:
+        current_time = datetime.now()
+        retry_at = current_time + timedelta(seconds=self.RETRY_DELAY_AFTER_502)
+        self.five02_domains[url_domain] = retry_at
+        self._scrape_url_at(url, retry_at)
+    else:
+      reason = response.status_code
+      self._add_url_to_bad_urls_file(url, reason)
+
+  def _scrape_url_at(self, url, scrape_at):
+    priority = int(scrape_at.timestamp())
+    self.urls.put((priority, url))
 
   def _sleep_until(self, target_time):
     current_time = datetime.now()
@@ -84,14 +126,6 @@ class Scraper:
     delta = (target_time - current_time).total_seconds()
     time.sleep(delta)
 
-  def _scrape_url_at(self, url, scrape_at):
-    priority = int(scrape_at.timestamp())
-    self.urls.put((priority, url))
-
-  def _scrape_url_in(self, url, seconds):
-    current_time = datetime.now()
-    self._scrape_url_at(url, current_time + timedelta(seconds=seconds))
-
   def _add_url_to_processed_urls_file(self, url):
       line = f"{url}, {self._formatted_current_time()}\n"
       self.processed_urls_file.write(line)
@@ -99,6 +133,9 @@ class Scraper:
   def _add_url_to_bad_urls_file(self, url, reason):
     line = f"{url}, {self._formatted_current_time()}, {reason}\n"
     self.bad_urls_file.write(line)
+
+  def _formatted_current_time(self):
+    return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
 def process_page(input_html, domain, url_path):
   pass
